1e7a145babc1d0f9d4eddfe78487ba89d2210f74
iamsarb001 <sarbbsandhu555@gmail.com>
2025-06-15 13:34:59 +0530

    oauth tokens working

--- backend/Routes/YtRoute.js
+++ backend/Routes/YtRoute.js
@@ -1,5 +1,5 @@
 import express from 'express'
-
+import axios from 'axios';
 
 const router = express.Router();
 
@@ -16,11 +16,75 @@ router.get('/login' , async(req,res) => {
 });
 
 
-router.post('/login' , async(req,res) => {
+router.get('/auth/youtube' , async(req,res,next)   => {
+   try {
+    const Authurls = `${process.env.AUTH_URL}?client_id=${process.env.CLIENT_ID}&redirect_uri=${process.env.Redirect_uri}&response_type=code&scope=${process.env.Scope}&access_type=offline&prompt=consent`;
+
+        console.log('Auth url is -',Authurls);
+        res.json({  url : Authurls })
+
+   } catch (error) {
+     console.log('error =',error);
+      return res.status(500).json({
+        message : "Rdirection failed"
+      })
+   }
+})
+
+router.get('/callbackurl' , async(req,res,next) => {
+    try {
+        const code = req.query.code;
+        console.log('code query -',code);
+  
+        const Resp = await axios.post(process.env.Token_url,
+          {
+           code,
+           client_id : process.env.CLIENT_ID,
+           client_secret : process.env.CLIENT_SECRET,
+           redirect_uri: process.env.Redirect_uri,
+           grant_type : 'authorization_code',
+        }, { headers : { 'Content-Type' : 'application/json' }})
+  
+        const { access_token , refresh_token } = Resp?.data;
+        console.log(' tokens are =',access_token , refresh_token);
+
+        return res.status(200).json({
+            message : "got the token"
+        })
+        
+    } catch (error) {
+        return res.status(500).json({
+            message : "token fetch error "
+        })
+     }
+})
+
+router.get('/videoid' , async(req,res) => {
      try {
-        
-     } catch (error) {
-        
+        const videoid = 'C9Ha9aRvJaA';
+        const Accesstoken = '';
+
+        const Response = await axios.get('/videos',{
+            params : {
+                part : 'snippet,contentDetails,statistics',
+                id : videoid,
+                key : process.env.API_KEY
+            },
+            headers : {
+                'Content-Type' : 'application/json',
+                 'Authorization' : `Bearer ${Accesstoken}`
+            }
+        })
+
+        //  console.log('api-key  =',process.env.API_KEY);
+
+        return res.status(200).json({
+            message : " Fetched video details "
+        })
+    } catch (error) {
+         return res.status(500).json({
+             message : " Video Error "
+         })
      }
 })
 
--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,14 @@
 import express from 'express';
 import router from './Routes/YtRoute.js';
 const app = express();
+import dotenv from 'dotenv'
+
+dotenv.config();
 
 app.use(express.json());
-app.use('/',router);
+app.use('/api/v1',router);
 
-app.get('/api/v1' , async(req,res) => {
- return res.status(200).json({
-    message : "Backend  working"
-  })
-})
-
-const PORT = 3000;
+const PORT = 4000;
 
 app.listen(PORT , () => {
     console.log(`Server is listening on ${PORT}..`)
