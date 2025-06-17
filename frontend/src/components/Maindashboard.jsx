f72ce903efdc11cba1139ecbda6ac8c63036f327
iamsarb001 <sarbbsandhu555@gmail.com>
2025-06-17 06:57:37 +0530

    updating content |tokens -implemented

--- frontend/src/components/Maindashboard.jsx
+++ frontend/src/components/Maindashboard.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useSearchParams } from "react-router-dom"
+import { useNavigate, useSearchParams } from "react-router-dom"
 import axios from 'axios';
 
 export const Maindashboard = () => {
@@ -10,26 +10,40 @@ export const Maindashboard = () => {
     const[refreshtoken,setrefreshtoken] = useState('');
     
     const[VideoResult,setVideoResult] = useState({});
+    const[Newtitle,setNewtitle] = useState('');
+    const[NewDesc,setNewDesc]   = useState('');
+    const[showinputbar,setshowinputbar] = useState(false);
+
+    const navigate = useNavigate();
 
     useEffect(() => {
 
         const acctoken = params.get('accesstoken');
-        console.log('acc token =',acctoken);
-        setAccesstoken(acctoken);
-    
-        console.log('ref token = ',params.get('refreshtoken'));
         const reftoken = params.get('refreshtoken');
-        setrefreshtoken(reftoken);
-        
-        localStorage.setItem('access_token',acctoken);
-        localStorage.setItem('refresh_token',reftoken);
-        
-    },[params])
+
+        console.log('Accesstoken =',Accesstoken);
+        console.log('refreshtoken =',refreshtoken);
+        if(acctoken && reftoken){
+            localStorage.setItem('access_token',acctoken);
+            localStorage.setItem('refresh_token',reftoken);
+            
+            console.log(' at enddddddd',);
+            navigate(window.location.pathname,{  replace : true });
+        }
+
+    },[navigate])
+
+    useEffect(() => {
+        const Acctoken = localStorage.getItem('access_token');
+        setAccesstoken(Acctoken);
+        const Reftoken = localStorage.getItem('refresh_token');
+        setrefreshtoken(Reftoken);
+    },[])
 
 
     useEffect(() => {
         console.log('Acc token =',Accesstoken);
-        
+
          async function fetchvideo(){
             if(Accesstoken){
                 const Response = await axios.get('/api/v1/mychannel',{
@@ -52,7 +66,31 @@ export const Maindashboard = () => {
             }
          }
          fetchvideo();
-    },[Accesstoken])
+    },[Accesstoken,Newtitle])
+
+    const Titledithandler = () => {
+        //  show input bar 
+        setshowinputbar((prev) => !prev);
+    }
+
+    const updatetitlehandler = async() => {
+        console.log('new title-',Newtitle);
+        const NewDesc = "aujla desc"
+         setshowinputbar((prev) => !prev);
+
+        const UpdateResponse = await axios.put('/api/v1/content',{
+            title : Newtitle,
+            desc  : NewDesc
+        },{
+            headers : {
+                'Content-Type' : 'application/json',
+                'Authorization' : `Bearer ${Accesstoken}`
+            }
+        })
+        console.log('Update Response =',UpdateResponse);
+
+    }
+
 
     return (
         <div className="px-4">
@@ -61,8 +99,27 @@ export const Maindashboard = () => {
                 <div className="w-[200px] h-[200px]">
                    <img src =  {VideoResult?.Imgurl}  alt = {VideoResult?.title} />
                 </div>
-                   <h2> Title - {VideoResult?.title} </h2>
-                   <h3> Description - {VideoResult?.desc} </h3>
+                   <h2> Present Title - {VideoResult?.title} </h2>
+                   <div>
+                      {showinputbar &&  (<> 
+                      <input type = "text" placeholder = "Enter new title" value = {Newtitle} onChange={(e) => setNewtitle(e.target.value)} /> 
+                       </>)}
+
+                       {showinputbar ? (<> 
+                       <button className="bg-black text-white px-4 py-2 rounded-xs" 
+                      onClick={updatetitlehandler}
+                      > update title </button>
+                       </>) : (<>
+                       
+                       <button className="bg-black text-white px-4 py-2 rounded-xs" 
+                      onClick={Titledithandler}
+                      > Edit title </button> </>)}
+
+                   </div>
+                   <div>
+                      
+                   </div>
+                   {/* <h3> Description - {VideoResult?.desc} </h3> */}
             </div>
         </div>
     )
